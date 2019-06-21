dd74688f34ccdaec4d68d0e8eb60c0f693859b6c
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-21 16:39:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,6 +9,7 @@ end
 
 def shipping_manifest(array)
   the_manifest = {"whale bone corset", " whale bone corset", "whale bone corset", "whale bone corset", "whale bone corset", "porcelain vase", "porcelain vase", "oil painting", "oil painting", "oil painting"}
+  the_manifest
   
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
